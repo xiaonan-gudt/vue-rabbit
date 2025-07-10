121b94f79d914f9e69c61cd8424be6f7fd9bd65c
xiaonan <1149248284@qq.com>
2025-07-10 18:55:48 +0800

    elementplus自动按需导入成功

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -7,6 +9,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
     vue(),
     vueDevTools(),
   ],
