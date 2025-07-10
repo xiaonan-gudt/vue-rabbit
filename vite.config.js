2337351b7c7eee6a38022ba5c2123dede6d33130
xiaonan <1149248284@qq.com>
2025-07-10 21:43:50 +0800

    sass配置完成

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,9 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({
+        importStyle:'sass'
+      })],
     }),
     vue(),
     vueDevTools(),
@@ -23,4 +25,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
