5f4e3199b8aefce0ffc7403eaf95edd3122d9ff4
xiaonan <1149248284@qq.com>
2025-07-11 01:45:01 +0800

    scss自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,11 @@
 <template>
     <el-button type="primary">Primary</el-button>
     <router-view />
+    <div class="test">test</div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.test{
+    color: $priceColor;
+}
 </style>
