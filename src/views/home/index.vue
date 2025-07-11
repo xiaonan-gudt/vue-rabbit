a6a429ecc7800f0bc6bcc6cf92c29c7b0afeae9f
xiaonan <1149248284@qq.com>
2025-07-12 01:03:45 +0800

    homepanel完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,6 +4,7 @@ import HomeProduct from "@/components/homeProduct.vue";
 import HomeNew from "@/components/homeNew.vue";
 import HomeBanner from "@/components/homeBanner.vue";
 import HomeCategory from "@/components/homeCategory.vue";
+import HomePanel from  "@/components/homePanel.vue"
 </script>
 <template>
    <div class="container">
@@ -13,5 +14,7 @@ import HomeCategory from "@/components/homeCategory.vue";
     <HomeNew />
     <HomeHot />
     <HomeProduct />
+    <HomePanel title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
+    <div>主体内容区域</div> </HomePanel>
 </template>
  
