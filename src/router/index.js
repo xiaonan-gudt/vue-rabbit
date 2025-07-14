60c5b672ca8a86aa57d92334be2bd62352ab4cb1
xiaonan <1149248284@qq.com>
2025-07-14 16:33:25 +0800

    二级分类页路由配置以及页面渲染初步完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Layout from '@/views/layout/index.vue'
 import Home from '@/views/home/index.vue'
 import Login from '@/views/login/index.vue'
 import Category from '@/views/category/index.vue'
-
+import SubCategory from '@/views/subCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +23,11 @@ const router = createRouter({
           path:'/category/:id',
           name:'category',
           component:Category,
+        },
+        {
+          path:'/category/sub/:id',
+          name:'subCategory',
+          component:SubCategory,
         }
       ]
     },
