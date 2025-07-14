42ab0a6f3492111e6652135d26ecc095f8251052
xiaonan <1149248284@qq.com>
2025-07-14 17:23:34 +0800

    二级分类页--面包屑导航渲染完成

--- src/views/subCategory/index.vue
+++ src/views/subCategory/index.vue
@@ -1,6 +1,15 @@
 <script setup>
-
-
+import {getCategoryFilterAPI} from '@/apis/category'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const subCategoryData = ref({})
+const getSubCategoryList = async (id = route.params.id) =>{
+  const res = await getCategoryFilterAPI(id)
+  console.log(res)
+  subCategoryData.value = res.data.result
+}
+onMounted(()=>getSubCategoryList())
 </script>
 
 <template>
@@ -9,9 +18,9 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家
+        <el-breadcrumb-item :to="{ path: '/' }">{{subCategoryData.parentName}}
         </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+        <el-breadcrumb-item>{{subCategoryData.name}}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
